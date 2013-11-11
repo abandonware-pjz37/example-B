33f75a7b876c8fdd8f96b6fb989c8f39f23a4d44
Ruslan Baratov <ruslan_baratov@yahoo.com>
2013-11-11 21:03:10 +0400

    Add install

--- cmake/FindExample_A.cmake
+++ cmake/FindExample_A.cmake
@@ -4,14 +4,14 @@ if(NOT EXAMPLE_A_ROOT)
   message(FATAL_ERROR "Please define EXAMPLE_A_ROOT")
 endif()
 
-if(NOT EXISTS ${EXAMPLE_A_ROOT}/Install/example_A/foo.hpp)
+if(NOT EXISTS ${EXAMPLE_A_ROOT}/example_A/foo.hpp)
   message(FATAL_ERROR "File ${EXAMPLE_A_ROOT}/example_A/foo.hpp not found")
 endif()
 
-set(EXAMPLE_A_INCLUDE_DIRS ${EXAMPLE_A_ROOT}/Install)
+set(EXAMPLE_A_INCLUDE_DIRS ${EXAMPLE_A_ROOT})
 
-find_library(EXAMPLE_A_LIBRARY example_A PATHS ${EXAMPLE_A_ROOT}/Install)
+find_library(EXAMPLE_A_LIBRARY example_A PATHS ${EXAMPLE_A_ROOT})
 
 if(NOT EXAMPLE_A_LIBRARY)
-  message(FATAL_ERROR "Library example_A not found in ${EXAMPLE_A_ROOT}/Install")
+  message(FATAL_ERROR "Library example_A not found in ${EXAMPLE_A_ROOT}")
 endif()
